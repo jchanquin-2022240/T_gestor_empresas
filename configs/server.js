55c71e265d841a46b72211d2ec4b240df27b1290
jchanquin-2022240 <jchanquin-2022240@kinal.edu.gt>
2024-02-27 09:49:06 -0600

    Login funcional y se agrego la conllection del mismo

--- configs/server.js
+++ configs/server.js
@@ -4,6 +4,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import adminRoutes from '../src/admin/admin.routes.js';
 import enterpriseRoutes from '../src/enterprises/enterprise.routes.js';
+import authRoutes from '../src/auth/auth.routes.js';
 import { dbConnection } from './mongo.js';
 
 class Server {
@@ -12,6 +13,7 @@ class Server {
         this.port = process.env.PORT;
         this.adminPath = '/COPEREX/v1/admin';
         this.enterprisePath = '/COPEREX/v1/enterprise';
+        this.authPath = '/COPEREX/v1/auth'
 
         this.conectarDB();
         this.middlewares();
@@ -31,6 +33,7 @@ class Server {
     }
 
     routes() {
+        this.app.use(this.authPath, authRoutes);
         this.app.use(this.adminPath, adminRoutes);
         this.app.use(this.enterprisePath, enterpriseRoutes);
     }
